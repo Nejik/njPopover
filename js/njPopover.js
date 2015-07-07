58024f56b844da8f3f848b40b4db857719fd113d
Dmitry <nejikrofl@gmail.com>
2015-07-07 14:37:23 +0300

    position absolute not required in o.template

--- js/njPopover.js
+++ js/njPopover.js
@@ -857,7 +857,7 @@ njPopover.defaults = {
 	margin: 5,//(number) margin from element
 
 
-	template:'<div class="njp-wrap" data-njp-wrap style="position:absolute;"><div class="njp" data-njp></div></div>',//(string) base HTML to use when creating the popover
+	template:'<div class="njp-wrap" data-njp-wrap><div class="njp" data-njp></div></div>',//(string) base HTML to use when creating the popover
 	attr: 'title',//get content for popover from this attribute, if there is no o.content option
 	type: 'html',//(html || selector || text) type of content, if selector used, whole element will be inserted in tooltip
 	content: '',//(string || function) content for popover
